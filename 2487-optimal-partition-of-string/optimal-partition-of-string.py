05e726c6a0f1a55e6a0695c4fe549281080b4651
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-02-02 22:05:44 +0530

    Time: 47 ms (79.06%) | Memory: 18.2 MB (34.87%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -1,12 +1,12 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        hash1 = set()
+        hash1 = []
         count = 1
         for i in s :
             if i in hash1:
                 count+=1
                 hash1.clear()
-            hash1.add(i)
+            hash1.append(i)
         
 
         return count
