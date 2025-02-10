429fec98ea1faede11ce9d5706d4ca85bed10a6e
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-02-10 22:43:46 +0530

    Time: 660 ms (7.43%) | Memory: 18.8 MB (71.77%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,20 +1,14 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
+
         """
         Do not return anything, modify nums in-place instead.
         """
-        l = 0  # Pointer for non-zero elements
-        r = 1  # Pointer for searching non-zero elements
-
-        while r < len(nums):  # Iterate through the list
-            if nums[l] == 0 and nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]  # Swap zero with non-zero
-                l += 1  # Move l to next position
-            elif nums[l] != 0:
-                l += 1  # Move l if it's already a non-zero number
-            r += 1  # Always move r forward
-        
-        return nums
+        n = len(nums)
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                nums.append(nums[i])                    
+                nums.remove(nums[i])
             
             
         
