df6228efee9c0b97e8e3f47b0afb2fd63dd2f90b
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-05-22 21:24:26 +0530

    Time: 42 ms (51.39%) | Memory: 54 MB (20.21%) - LeetSync

--- 2775-generate-fibonacci-sequence/generate-fibonacci-sequence.js
+++ 2775-generate-fibonacci-sequence/generate-fibonacci-sequence.js
@@ -1,7 +1,20 @@
-function* fibGenerator() {
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
     let a = 0, b = 1;
     while (true) {
-        yield a;
-        [a, b] = [b, a + b];  // Move to the next Fibonacci number
+        yield a;     // yield current Fibonacci number
+        [a, b] = [b, a + b]; // move forward in sequence
     }
-}
+};
+
+/**
+ * const gen = fibGenerator();
+ * console.log(gen.next().value); // 0
+ * console.log(gen.next().value); // 1
+ * console.log(gen.next().value); // 1
+ * console.log(gen.next().value); // 2
+ * console.log(gen.next().value); // 3
+ * // and so on...
+ */
