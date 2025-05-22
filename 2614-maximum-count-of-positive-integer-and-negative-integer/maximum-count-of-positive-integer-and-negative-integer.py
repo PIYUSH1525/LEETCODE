5baf5148dd4889c1e412c678dd446ce303ac88b2
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-05-22 21:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (25.76%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,10 +1,34 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        pos = 0
-        neg = 0
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                pos+=1
-            elif nums[i]< 0:
-                neg+=1
-        return max(pos,neg)
+        def find_first_zero():
+            l = 0 
+            r= len(nums)
+            while l<r:
+                mid = (l+r) //2
+                if nums[mid] < 0:
+                    l = mid+1
+                else :
+                    r = mid
+            return l 
+        def find_first_pos():
+            l = 0 
+            r= len(nums)
+            while l<r:
+                mid = (l+r) //2
+                if nums[mid] <= 0:
+                    l = mid+1
+                else :
+                    r = mid
+            return l 
+        neg_count = find_first_zero()
+        pos_count = len(nums) - find_first_pos()
+        return max(neg_count, pos_count)
+
+        # pos = 0
+        # neg = 0
+        # for i in range(len(nums)):
+        #     if nums[i] > 0:
+        #         pos+=1
+        #     elif nums[i]< 0:
+        #         neg+=1
+        # return max(pos,neg)
