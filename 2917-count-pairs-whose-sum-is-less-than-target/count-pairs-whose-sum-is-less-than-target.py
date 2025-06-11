7ff00077f8f26a758cd31317372c28362bd1542a
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-06-11 09:07:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (56.70%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.py
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.py
@@ -1,10 +1,21 @@
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
-        count = 0
-        for i in range (len(nums)):
-            for j in range (i+1,len(nums)):
-                if   nums[i]+ nums[j] < target :
-                    count+=1
-        return count
+        # count = 0
+        # for i in range (len(nums)):
+        #     for j in range (i+1,len(nums)):
+        #         if   nums[i]+ nums[j] < target :
+        #             count+=1
+        # return count
+        low=0
+        nums.sort()
+        high=len(nums)-1
+        pair=0
+        while(low<high):
+            if(nums[low]+nums[high]<target):
+                pair+=high-low
+                low+=1
+            else:
+                high-=1
+        return pair
 
         
