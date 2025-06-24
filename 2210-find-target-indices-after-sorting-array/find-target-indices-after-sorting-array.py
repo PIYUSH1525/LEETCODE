d84c53f4b911a93c4fdaab29cafff9b62c095b2c
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-06-24 23:56:59 +0530

    Time: 4 ms (7.81%) | Memory: 17.7 MB (57.30%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
@@ -3,7 +3,15 @@ class Solution:
         nums.sort()
         res = []
         # brute force using loop O(n)
-        for i in range(len(nums)):
-            if nums[i] == target:
-                res.append(i)
-        return res
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         res.append(i)
+        # return res
+        # using Counting
+        less = equal = 0
+        for num in nums:
+            if num < target:
+                less += 1
+            elif num == target:
+                equal += 1
+        return list(range(less, less + equal))
