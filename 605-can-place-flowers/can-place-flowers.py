908774fcd6a3a7c28e95d063245150e9c50c9531
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-07-21 15:05:14 +0530

    Time: 6 ms (78.92%) | Memory: 18.3 MB (14.59%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,20 +1,21 @@
+from typing import List
+
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        i = 0
-        length = len(flowerbed)
+        count = 1 
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                count += 1
+            else:
+                count = 0
 
-        while i < length:
-            if (
-                flowerbed[i] == 0 and
-                (i == 0 or flowerbed[i - 1] == 0) and
-                (i == length - 1 or flowerbed[i + 1] == 0)
-            ):
-                flowerbed[i] = 1
+            if count == 3:
                 n -= 1
+                count = 1 
+
                 if n == 0:
                     return True
-                i += 2  # Skip the next spot since it's adjacent
-            else:
-                i += 1  # Move to the next spot
+        if count == 2:
+            n -= 1
 
         return n <= 0
