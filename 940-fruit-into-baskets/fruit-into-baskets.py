10537a155d07d1b9afad7c5deae6c66c96c5d9d1
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-08-04 21:03:28 +0530

    Time: 170 ms (78.92%) | Memory: 23.6 MB (42.94%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -11,42 +11,6 @@ class Solution:
                     del hashmap[fruits[l]]
                 l+=1
             maxx = max(maxx, r - l + 1)
-
         return maxx
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        #     if fruits[r] not in hashmap and len(hashmap) <2:
-        #         hashmap[fruits[r]]
-        #         r+=1
-        #         curr = (r-l)+1
-        #         maxx = max(maxx, curr)
-        #     if fruits[r] in hashmap:
-        #         fruits[r].value+=1
-        #         r+=1
-        #         curr = (r-l)+1
-        #         maxx = max(maxx, curr)
-                
-        #     else:
-        #         a.remove(fruits[l])
-        #         l+=1
-        # return maxx
-        
-        
