f83b9573a003c8d1b6eafee7b7825d95ca5d5bd3
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-08-06 21:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (63.10%) - LeetSync

--- 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.py
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.py
@@ -1,22 +1,22 @@
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
         count_zero = 0
-        count_one =0
-        l,r=0,0
+        count_one = 0
+        l,r = 0,0
         res =0
-        while r <len(s): 
-            if s[r] =='0':
+        while r <len(s):
+            if s[r] == "0":
                 count_zero+=1
-            else:
+            if s[r] =="1":
                 count_one+=1
-            while count_zero>k and count_one>k:
-                if s[l] =='0':
+            while count_zero>k and count_one>k :
+                if s[l] =="0":
                     count_zero-=1
-                else:
+                if s[l] =="1":
                     count_one-=1
-                l+=1
-            res += (r - l) + 1
+                l+=1  
+            curr = (r-l)+1
+            res += curr  
             r+=1
+            
         return res
-           
-        
