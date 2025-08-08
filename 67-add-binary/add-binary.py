eaa59870158f1586ce2dd38de265d8e62b92d511
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-08-08 22:43:37 +0530

    Time: 4 ms (17.48%) | Memory: 17.9 MB (27.59%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -5,15 +5,11 @@ class Solution:
         carry =0
         res = []
         while i>=0 or j >=0 or carry>0:
-            digit_a = int(a[i]) if i>=0 else 0
-            digit_b = int(b[j]) if j >=0 else 0
-            summ = digit_a + digit_b + carry
+            dig_a = a[i] if i>=0 else 0
+            dig_b = b[j] if j>=0 else 0
+            summ = int(dig_a)+ int(dig_b)+ carry
             res.append(str(summ%2))
-            carry = summ//2 
+            carry = summ//2
             i-=1
             j-=1
-        # if carry >0:
-        #     res.append(carry)
         return "".join(res[::-1])
-
-        
