b7bfb66db5dc10193f486642ad0d809c738c37b3
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2025-08-14 22:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.96%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -1,19 +1,31 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        prev = "A"
-        count =  0
-        res = []
-        if len(num) <3:
+        l = 0
+        count = 0
+        res =[]
+        for r in range(len(num)):
+            if num[l]!= num[r]:
+                l=r
+            if r - l + 1 == 3 and num[l] == num[r]:
+                res.append(num[l])
+        if not res:
             return ""
-        for i in range(len(num)):
-            if num[i] == prev:
-                count+=1
-            else:
-                prev = num[i]
-                count=1
-            if count == 3:
-                res.append(num[i])
-        if len(res) ==0:
-            return ""
-        z = max(res),max(res),max(res)
-        return("".join(z))
+        max_digit = max(res)
+        return max_digit * 3
+        # prev = "A"
+        # count =  0
+        # res = []
+        # if len(num) <3:
+        #     return ""
+        # for i in range(len(num)):
+        #     if num[i] == prev:
+        #         count+=1
+        #     else:
+        #         prev = num[i]
+        #         count=1
+        #     if count == 3:
+        #         res.append(num[i])
+        # if len(res) ==0:
+        #     return ""
+        # z = max(res),max(res),max(res)
+        # return("".join(z))
