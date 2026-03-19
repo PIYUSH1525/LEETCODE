ae6eea0e03db863328158e6a9389723d1edbd019
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-03-19 12:31:48 +0530

    Time: 8 ms (20.96%) | Memory: 21 MB (86.24%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hashMap = Counter(nums)
-        # nums: freq
-        for x in hashMap:
-            if hashMap[x] ==1:
-                return x
+        ans = nums[0]
+        for i in range(1,len(nums)):
+            ans = ans ^ nums[i]
+        return ans
