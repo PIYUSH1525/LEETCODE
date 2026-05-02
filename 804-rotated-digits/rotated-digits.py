352e9bd22ad6aad309df01ea8f0ac1bdc071640a
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-05-02 09:33:11 +0530

    Time: 2 ms (92.13%) | Memory: 19.4 MB (14.49%) - LeetSync

--- 804-rotated-digits/rotated-digits.py
+++ 804-rotated-digits/rotated-digits.py
@@ -1,18 +1,10 @@
+nums = []
+for n in range(1, 10**4+1):
+    s = str(n)
+    if ("2" in s or "5" in s or "6" in s or "9" in s) and ("3" not in s and "4" not in s and "7" not in s):
+        nums.append(n)
+
 class Solution:
     def rotatedDigits(self, n: int) -> int:
-      
-        def is_good_number(number):
-            rotated_number = 0
-            temp_number = number
-            place_value = 1
-          
-            while temp_number:
-                digit = temp_number % 10
-                if rotation_map[digit] == -1:
-                    return False
-                rotated_number = rotation_map[digit] * place_value + rotated_number
-                place_value *= 10
-                temp_number //= 10
-            return number != rotated_number
-        rotation_map = [0, 1, 5, -1, -1, 2, 9, -1, 8, 6]
-        return sum(is_good_number(i) for i in range(1, n + 1))
+        res = 0
+        return bisect.bisect(nums, n) 
