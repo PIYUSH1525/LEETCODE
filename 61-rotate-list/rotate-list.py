e89d4c9c0c5201320f50fab4897b250b155988dc
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-05-05 09:12:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.76%) - LeetSync

---
+++ 61-rotate-list/rotate-list.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        current = head
+        list_length = 0
+        while current:
+            list_length += 1
+            current = current.next
+        k = k % list_length
+        if k == 0:
+            return head
+        fast_pointer = head
+        slow_pointer = head
+        for _ in range(k):
+            fast_pointer = fast_pointer.next
+        while fast_pointer.next:
+            fast_pointer = fast_pointer.next
+            slow_pointer = slow_pointer.next
+        new_head = slow_pointer.next
+        slow_pointer.next = None
+        fast_pointer.next = head
+      
+        return new_head
