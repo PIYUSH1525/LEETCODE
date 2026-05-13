d0176bff14897cf1fcd17b951b5c8b063bb0e364
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-05-13 09:45:39 +0530

    Time: 196 ms (63.08%) | Memory: 31 MB (60.00%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.py
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.py
@@ -1,19 +1,23 @@
-from typing import List
-from itertools import accumulate
 class Solution:
     def minMoves(self, nums: List[int], limit: int) -> int:
-        diff_array = [0] * (2 * limit + 2)
         n = len(nums)
+        diff = [0] * (2 * limit + 2)
+
         for i in range(n // 2):
-            left_val = nums[i]
-            right_val = nums[n - 1 - i]
-            if left_val > right_val:
-                left_val, right_val = right_val, left_val
-            diff_array[2] += 2
-            diff_array[left_val + 1] -= 2
-            diff_array[left_val + 1] += 1
-            diff_array[left_val + right_val] -= 1
-            diff_array[left_val + right_val + 1] += 1
-            diff_array[right_val + limit + 1] -= 1
-            diff_array[right_val + limit + 1] += 2
-        return min(accumulate(diff_array[2:]))
+            a = nums[i]
+            b = nums[n - 1 - i]
+            low = min(a, b) + 1
+            high = max(a, b) + limit
+            total = a + b
+            diff[2] += 2
+            diff[2 * limit + 1] -= 2
+            diff[low] -= 1
+            diff[high + 1] += 1
+            diff[total] -= 1
+            diff[total + 1] += 1
+        ans = float('inf')
+        moves = 0
+        for target in range(2, 2 * limit + 1):
+            moves += diff[target]
+            ans = min(ans, moves)
+        return ans
