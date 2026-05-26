a15b2f2d1b7076bdfe8cd223a3587bbec5aa2b9f
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-05-26 11:22:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (64.16%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
@@ -1,14 +1,4 @@
 class Solution:
-  def numberOfSpecialChars(self, word: str) -> int:
-    lower = collections.defaultdict(bool)
-    upper = collections.defaultdict(bool)
-
-    for c in word:
-      if c.islower():
-        lower[c] = True
-      else:
-        upper[c] = True
-
-    return sum(lower[a] and upper[b]
-               for a, b in zip(string.ascii_lowercase,
-                               string.ascii_uppercase))
+    def numberOfSpecialChars(self, word: str) -> int:
+        s = set(word)
+        return sum(a in s and b in s for a, b in zip(ascii_lowercase, ascii_uppercase))
