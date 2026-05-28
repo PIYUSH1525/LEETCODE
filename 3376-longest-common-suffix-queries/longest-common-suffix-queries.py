7893b3de8b783354cfecf92b736635ce2915f07a
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-05-28 16:26:31 +0530

    Time: 1477 ms (49.69%) | Memory: 149.7 MB (86.79%) - LeetSync

--- 3376-longest-common-suffix-queries/longest-common-suffix-queries.py
+++ 3376-longest-common-suffix-queries/longest-common-suffix-queries.py
@@ -1,36 +1,42 @@
 class TrieNode:
-  def __init__(self):
-    self.children: dict[str, TrieNode] = {}
-    self.isWord = False
-    self.length = math.inf
-    self.index = -1
+    __slots__ = ['children', 'bestLen', 'bestIdx']
+    
+    def __init__(self):
+        self.children = {}
+        self.bestLen = float('inf')
+        self.bestIdx = float('inf')
 class Solution:
-  def stringIndices(self,wordsContainer: list[str],wordsQuery: list[str],) -> list[int]:
-    ans = []
-    root = TrieNode()
-    minIndex = min(enumerate(wordsContainer), key=lambda x: len(x[1]))[0]
-
-    def insert(word: str, index: int) -> None:
-      node = root
-      for c in reversed(word):
-        node = node.children.setdefault(c, TrieNode())
-        if node.length > len(word):
-          node.length = len(word)
-          node.index = index
-
-    def search(word: str) -> int:
-      node = root
-      for c in reversed(word):
-        if c not in node.children:
-          return node.index
-        node = node.children[c]
-      return node.index
-
-    for i, word in enumerate(wordsContainer):
-      insert(word, i)
-
-    for query in wordsQuery:
-      index = search(query)
-      ans.append(minIndex if index == -1 else index)
-
-    return ans
+    def stringIndices(self, wordsContainer: List[str], wordsQuery: List[str]) -> List[int]:
+        root = TrieNode()
+        
+        for i, word in enumerate(wordsContainer):
+            n = len(word)
+            curr = root
+            
+            if n < curr.bestLen or (n == curr.bestLen and i < curr.bestIdx):
+                curr.bestLen = n
+                curr.bestIdx = i
+                
+            for char in reversed(word):
+                if char not in curr.children:
+                    curr.children[char] = TrieNode()
+                
+                curr = curr.children[char]
+                
+                if n < curr.bestLen or (n == curr.bestLen and i < curr.bestIdx):
+                    curr.bestLen = n
+                    curr.bestIdx = i
+                    
+        ans = []
+        
+        for query in wordsQuery:
+            curr = root
+            
+            for char in reversed(query):
+                if char not in curr.children:
+                    break
+                curr = curr.children[char]
+            
+            ans.append(curr.bestIdx)
+            
+        return ans
