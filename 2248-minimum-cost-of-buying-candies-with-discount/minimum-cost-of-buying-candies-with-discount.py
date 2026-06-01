45701df7160320cf7f6c3862cc48bd33cd48fbec
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-06-01 10:43:50 +0530

    Time: 3 ms (27.74%) | Memory: 19.3 MB (48.06%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        cost.sort(reverse=True)
+        total_cost = sum(cost) - sum(cost[2::3])
+      
+        return total_cost
