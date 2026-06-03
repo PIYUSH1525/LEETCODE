c370ede86ed16bf0571dec2231b76f0027bd9c7f
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-06-03 09:31:48 +0530

    Time: 135 ms (6.09%) | Memory: 33.9 MB (55.63%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.py
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -1,14 +1,18 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
-        asteroids.sort()
-        count = 0
-        n= len(asteroids)
-        for i in range(n):
-            if mass >= asteroids[i]:
-                mass+=asteroids[i]
-                count+=1
-        if count == n:
-            return True
-        else:
-            return False
+        max_ast = max(asteroids)
         
+        counts = [0] * (max_ast + 1)
+        for ast in asteroids:
+            counts[ast] += 1
+            
+        current_mass = mass
+        
+        for ast_mass in range(1, max_ast + 1):
+            if counts[ast_mass] > 0:
+                if current_mass < ast_mass:
+                    return False
+                
+                current_mass += ast_mass * counts[ast_mass]
+                
+        return True
