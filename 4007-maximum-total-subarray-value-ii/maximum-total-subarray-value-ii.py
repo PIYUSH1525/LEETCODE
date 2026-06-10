f111ac6a1505e64541f7a3a59e7518daa1c2ff66
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-06-10 09:55:48 +0530

    Time: 1073 ms (95.12%) | Memory: 53.1 MB (34.15%) - LeetSync

--- 4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.py
+++ 4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.py
@@ -1,36 +1,79 @@
+import heapq as hq
 class Solution:
-    def maxTotalValue(self, nums: list[int], k: int) -> int:
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        LUT = SparseTable(nums)
+        nums = [(v, i) for i, v in enumerate(nums)]
+        br = [n] * n 
+        stack = []
+        for i in range(n-1, -1, -1):
+            while stack and nums[stack[-1]] <= nums[i]:
+                stack.pop()
+            if stack:
+                br[i] = stack[-1]
+            stack.append(i)
 
-        pq = [(-LUT.query(i, n), i, n) for i in range(n)]
+        sr = [n] * n 
+        stack = []
+        for i in range(n-1, -1, -1):
+            while stack and nums[stack[-1]] >= nums[i]:
+                stack.pop()
+            if stack:
+                sr[i] = stack[-1]
+            stack.append(i)
 
-        res = 0
-        for _ in range(k):
-            val, l, r = pq[0]
-            if val == 0:
-                break
-            res -= val
-            heapq.heapreplace(pq, (-LUT.query(l, r - 1), l, r - 1))
-
-        return res
-
-class SparseTable:
-    def __init__(self, num: list[int]):
-        n = len(num)
-        bitWidth = n.bit_length()
-        self.Min = [[0] * n for _ in range(bitWidth)]
-        self.Max = [[0] * n for _ in range(bitWidth)]
-
+        bl = [-1] * n 
+        stack = []
         for i in range(n):
-            self.Min[0][i] = self.Max[0][i] = num[i]
+            while stack and nums[stack[-1]] <= nums[i]:
+                stack.pop()
+            if stack:
+                bl[i] = stack[-1]
+            stack.append(i)
+        sl = [-1] * n 
+        stack = []
+        for i in range(n):
+            while stack and nums[stack[-1]] >= nums[i]:
+                stack.pop()
+            if stack:
+                sl[i] = stack[-1]
+            stack.append(i)
 
-        for i in range(1, bitWidth):
-            for j in range(n - (1 << i) + 1):
-                self.Min[i][j] = min(self.Min[i - 1][j], self.Min[i - 1][j + (1 << (i - 1))])
-                self.Max[i][j] = max(self.Max[i - 1][j], self.Max[i - 1][j + (1 << (i - 1))])
-
-    def query(self, left: int, right: int) -> int:
-        k = (right - left).bit_length() - 1
-        return max(self.Max[k][left], self.Max[k][right - (1 << k)]) - \
-               min(self.Min[k][left], self.Min[k][right - (1 << k)])
+        ns = sorted(nums)
+        res = 0
+        heap = [(ns[0][0] - ns[-1][0], ns[0], ns[-1], 0, n-1)]
+        def ele(i, j):
+            return (ns[i][0] - ns[j][0], ns[i], ns[j], i, j)
+        def compose_next(i, j):
+            res = []
+            if i+1 <= j:
+                res.append(ele(i+1, j))
+            if i <= j-1:
+                res.append(ele(i, j-1))
+            return res
+        visited = set([heap[0]])
+        while k > 0:
+            gap, mn, mx, nsl, nsr = hq.heappop(heap)
+            for e in compose_next(nsl, nsr):
+                if e not in visited:
+                    hq.heappush(heap, e)
+                    visited.add(e)
+            mnv, mni = mn
+            mxv, mxi = mx
+            if mni <= mxi:
+                if sr[mni] <= mxi or bl[mxi] >= mni:
+                    continue
+                lb = max(sl[mni] + 1, bl[mxi] + 1)
+                rb = min(br[mxi] - 1, sr[mni] - 1)
+                cnt = min((mni - lb + 1) * (rb - mxi + 1), k)
+                res += cnt * abs(gap)
+                k -= cnt
+            else:
+                if br[mxi] <= mni or sl[mni] >= mxi:
+                    continue
+                lb = max(bl[mxi] + 1, sl[mni] + 1)
+                rb = min(sr[mni] - 1, br[mxi] - 1)
+                cnt = min((mxi - lb + 1) * (rb - mni + 1), k)
+                res += cnt * abs(gap)
+                k -= cnt
+            
+        return res
