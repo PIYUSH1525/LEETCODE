a7d46a6dc4afb7b99b06e85856736de6c08fcf43
Piyush Negi <120514593+PIYUSH1525@users.noreply.github.com>
2026-07-17 09:27:42 +0530

    Time: 388 ms (100.00%) | Memory: 40.1 MB (57.50%) - LeetSync

--- 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.py
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.py
@@ -1,22 +1,34 @@
-from typing import List
-from collections import Counter
-from itertools import accumulate
 from bisect import bisect_right
+from typing import List
 
 class Solution:
     def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
-        max_value = max(nums)
-        frequency_count = Counter(nums)
-        gcd_pair_count = [0] * (max_value + 1)
-        for gcd in range(max_value, 0, -1):
-            multiples_count = 0
-            for multiple in range(gcd, max_value + 1, gcd):
-                multiples_count += frequency_count[multiple]
-                gcd_pair_count[gcd] -= gcd_pair_count[multiple]
-            gcd_pair_count[gcd] += multiples_count * (multiples_count - 1) / 2
-
-        # Create prefix sum array for binary search
-        prefix_sum = list(accumulate(gcd_pair_count))
-
-        # For each query, find the GCD value using binary search on prefix sum
-        return [bisect_right(prefix_sum, query) for query in queries]
+        max_val = max(nums)
+        cnt = [0] * (max_val + 1)
+        for num in nums:
+            cnt[num] += 1
+            
+        gcd_cnt = [0] * (max_val + 1)
+        for i in range(max_val, 0, -1):
+            total_multiples = 0
+            for j in range(i, max_val + 1, i):
+                total_multiples += cnt[j]
+            
+            pairs = total_multiples * (total_multiples - 1) // 2
+            
+            for j in range(2 * i, max_val + 1, i):
+                pairs -= gcd_cnt[j]
+                
+            gcd_cnt[i] = pairs
+            
+        pref = [0] * (max_val + 1)
+        for i in range(1, max_val + 1):
+            pref[i] = pref[i - 1] + gcd_cnt[i]
+            
+        ans = []
+        for q in queries:
+            idx = bisect_right(pref, q)
+            ans.append(idx)
+            
+        return ans
+        
